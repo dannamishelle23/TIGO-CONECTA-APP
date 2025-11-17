9f4136f804ef4db3ac7dbac676cf7cb653a85b2f
dannamishelle23 <dannamishelle.23@gmail.com>
2025-11-17 15:44:09 -0500

    Actualizado environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://qaeloerwlxdzsyppappf.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZWxvZXJ3bHhkenN5cHBhcHBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0MDIxMDgsImV4cCI6MjA3ODk3ODEwOH0.roXOMDmHyQq72SA1gLU7jvvkebUCScMrH5QULKQjCdI'
 };
